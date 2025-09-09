fa3492eb0a8e4f85db95caf36e57bdbacbc8b09e
_hazielvds <user.haziel@gmail.com>
2025-09-09 09:44:27 -0600

    Added free challenge solution v1

--- src/MechanicsManager.py
+++ src/MechanicsManager.py
@@ -11,9 +11,9 @@ import SensorsManager
 # =========================
 # Constants
 # =========================
-CENTER_POSITION = 55  # Neutral position of the servo
-LEFT_POSITION = 45 # Left position of the servo
-RIGHT_POSITION = 65  # Right position of the servo
+CENTER_POSITION = 52  # Neutral position of the servo
+LEFT_POSITION = 62 # Left position of the servo
+RIGHT_POSITION = 42  # Right position of the servo
 is_turning = False
 
 # =========================
