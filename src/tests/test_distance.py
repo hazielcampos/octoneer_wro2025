6d6da83867bdf083111e17ebd34117f507f7bec8
_hazielvds <user.haziel@gmail.com>
2025-09-10 12:35:26 -0600

    idk

--- src/tests/test_distance.py
+++ src/tests/test_distance.py
@@ -1,10 +1,13 @@
-import adafruit_vl53l0x
-from utils.i2c_manager import i2c
-import libs.GPIO as GPIO
+from Components.DistanceSensor import DistanceSensor
 import time
 
-sensor = adafruit_vl53l0x.VL53L0X(i2c)
+sensor_right = DistanceSensor(xshut_pin=22, new_address=0x29)
+#sensor_left = DistanceSensor(xshut_pin=27, new_address=0x31)
+
+sensor_right.init_sensor()
+#sensor_left.init_sensor()
 
 while True:
-    print("Distance: {} mm".format(sensor.range))
-    time.sleep(0.5)
+    print(sensor_right.get_distance())
+    #print(sensor_left.get_distance())
+    time.sleep(0.001)
