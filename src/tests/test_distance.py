8d57cc14a3fdd91248340c82db6434602d794a50
_hazielvds <user.haziel@gmail.com>
2025-09-10 12:31:25 -0600

    idk

--- src/tests/test_distance.py
+++ src/tests/test_distance.py
@@ -1,11 +1,10 @@
-import VL53L0X
-
+import adafruit_vl53l0x
+from utils.i2c_manager import i2c
+import libs.GPIO as GPIO
 import time
 
-tof = VL53L0X.VL53L0X(i2c_bus=1, i2c_address=0x29)
-tof.open()
-tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+sensor = adafruit_vl53l0x.VL53L0X(i2c)
 
 while True:
-    distance = tof.get_distance()
-    print("Distance: {} mm".format(distance))
+    print("Distance: {} mm".format(sensor.range))
+    time.sleep(0.5)
