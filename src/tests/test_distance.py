6e75780971807499f33e413d3db4b576fddaab6b
_hazielvds <user.haziel@gmail.com>
2025-09-10 12:08:32 -0600

    Marked libs, components as modules

--- src/tests/test_distance.py
+++ src/tests/test_distance.py
@@ -4,10 +4,6 @@ import libs.GPIO as GPIO
 
 import time
 
-GPIO.setup(22, GPIO.OUT)
-GPIO.output(22, GPIO.LOW)   # Enciende el sensor
-time.sleep(0.05)
-
 sensor = adafruit_vl53l0x.VL53L0X(i2c)
 time.sleep(0.1)
 
