595a046f81b158c5082f96d12dcce47ade2b24cc
_hazielvds <user.haziel@gmail.com>
2025-09-10 12:22:53 -0600

    idk

--- src/tests/test_distance.py
+++ src/tests/test_distance.py
@@ -1,16 +1,14 @@
 import board
 import busio
-import adafruit_vl53l1x
-import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l1x.VL53L1X(i2c)
 
-vl53.distance_mode = 2   # 1=short, 2=long
-vl53.timing_budget = 200 # ms
+while not i2c.try_lock():
+    pass
 
-while True:
-    if vl53.data_ready:
-        print("Distance: {} mm".format(vl53.distance))
-        vl53.clear_interrupt()
-        time.sleep(0.1)
+addr = 0x29
+buf = bytearray(1)
+i2c.writeto_then_readfrom(addr, bytes([0xC0]), buf)  # registro de identificación
+print("Sensor ID:", hex(buf[0]))
+
+i2c.unlock()
