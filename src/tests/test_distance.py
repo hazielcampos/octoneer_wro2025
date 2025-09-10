713bce47f2161f5d6ffa49054a8e623308996a51
_hazielvds <user.haziel@gmail.com>
2025-09-10 14:06:53 -0600

    idk

--- src/tests/test_distance.py
+++ src/tests/test_distance.py
@@ -2,12 +2,12 @@ from Components.DistanceSensor import DistanceSensor
 import time
 
 sensor_right = DistanceSensor(xshut_pin=22, new_address=0x30)
-sensor_left = DistanceSensor(xshut_pin=27, new_address=0x31)
+#sensor_left = DistanceSensor(xshut_pin=27, new_address=0x31)
 
 sensor_right.init_sensor()
-sensor_left.init_sensor()
+#sensor_left.init_sensor()
 
 while True:
     print(sensor_right.get_distance())
-    print(sensor_left.get_distance())
+#    print(sensor_left.get_distance())
     time.sleep(0.001)
