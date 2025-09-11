3614dcc2dfc5aca15654a69173b3c6817b3e08c7
_hazielvds <user.haziel@gmail.com>
2025-09-10 20:06:23 -0600

    xd

--- src/tests/test_tca.py
+++ src/tests/test_tca.py
@@ -4,3 +4,11 @@ i2c = busio.I2C(board.SCL, board.SDA)
 tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
 
 print("TCA9548A presente en 0x70")  # si esto no da error, el TCA responde
+
+for ch in range(8):
+    if tca[ch].try_lock():
+        devices = tca[ch].scan()
+        tca[ch].unlock()
+        print(f"Canal {ch}: {devices}")
+    else:
+        print(f"No se pudo bloquear canal {ch}")
