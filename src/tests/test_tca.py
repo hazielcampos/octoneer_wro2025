a016cb0f90aeca702d259a239a1fc639f4d87019
_hazielvds <user.haziel@gmail.com>
2025-09-10 20:03:23 -0600

    xd

--- src/tests/test_tca.py
+++ src/tests/test_tca.py
@@ -3,9 +3,9 @@ import board, busio, adafruit_tca9548a
 i2c = busio.I2C(board.SCL, board.SDA)
 tca = adafruit_tca9548a.TCA9548A(i2c)
 
-if tca[1].try_lock():
-    devices = tca[1].scan()
-    tca[1].unlock()
+if tca[0].try_lock():
+    devices = tca[0].scan()
+    tca[0].unlock()
     print("Canal 1:", [hex(d) for d in devices])
 else:
     print("No se pudo bloquear el canal 0")
