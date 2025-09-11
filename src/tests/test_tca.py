118a4c6457d78a5a9c9c810075e650c63517dfba
_hazielvds <user.haziel@gmail.com>
2025-09-10 20:15:45 -0600

    xd

--- src/tests/test_tca.py
+++ src/tests/test_tca.py
@@ -9,7 +9,7 @@ tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
 print("TCA9548A presente en 0x70")  # si esto no da error, el TCA responde
 tcs = adafruit_tcs34725.TCS34725(tca[0])
 
-tcs.integration_time = 24
+tcs.integration_time = 50
 tcs.gain = 4
 
 vlx = adafruit_vl53l0x.VL53L0X(tca[1])
