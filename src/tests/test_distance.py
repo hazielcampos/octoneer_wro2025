0624ff3f054eee7f5bf3cabfe6634cccc8db9304
_hazielvds <user.haziel@gmail.com>
2025-09-10 19:53:00 -0600

    Added full tca control for sensors

--- src/tests/test_distance.py
+++ src/tests/test_distance.py
@@ -4,14 +4,16 @@ import time
 import adafruit_tca9548a
 import adafruit_tcs34725
 
-sensor_right = DistanceSensor(xshut_pin=22, new_address=0x30)
-sensor_left = DistanceSensor(xshut_pin=27, new_address=0x31)
+tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
+
+sensor_right = DistanceSensor(tca[1] ,xshut_pin=22, new_address=0x30)
+sensor_left = DistanceSensor(tca[2], xshut_pin=27, new_address=0x31)
 
 sensor_right.init_sensor()
 sensor_left.init_sensor()
 time.sleep(0.1)
 
-tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
+
 tcs = adafruit_tcs34725.TCS34725(tca[0])
 
 tcs.integration_time = 200
