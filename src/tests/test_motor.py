b2e896d25fdb1c62dacb8299d753aad569e36551
_hazielvds <user.haziel@gmail.com>
2025-09-11 15:46:36 -0600

    Ya saquenme de aquiii

--- src/tests/test_motor.py
+++ src/tests/test_motor.py
@@ -2,13 +2,17 @@
 from Components.Motor import forward, stop_motors, start as start_pwm
 import time
 
+
 def main():
     start_pwm()
-    forward(50)
-    input("Press Enter to stop motors...")
-    forward(0)
-    stop_motors()
-    time.sleep(0.1)
+    while True:
+        pwm = int(input("Enter pwm value:"))
+        
+        forward(pwm)
+        input("Press Enter to stop motors...")
+        forward(0)
+        stop_motors()
+        time.sleep(0.1)
     
 try:
     main()
