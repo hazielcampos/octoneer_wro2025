e4a1573c20a9fa6718bf97864744c7a18051ece8
_hazielvds <user.haziel@gmail.com>
2025-09-10 20:12:21 -0600

    xd

--- src/tests/test_tca.py
+++ src/tests/test_tca.py
@@ -1,14 +1,26 @@
 import board, busio, adafruit_tca9548a
+import adafruit_tcs34725
+import time
 
 i2c = busio.I2C(board.SCL, board.SDA)
 tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
 
 print("TCA9548A presente en 0x70")  # si esto no da error, el TCA responde
+tcs = adafruit_tcs34725.TCS34725(tca[0])
 
-for ch in range(8):
-    if tca[ch].try_lock():
-        devices = tca[ch].scan()
-        tca[ch].unlock()
-        print(f"Canal {ch}: {devices}")
-    else:
-        print(f"No se pudo bloquear canal {ch}")
+tcs.integration_time = 200
+tcs.gain = 4
+
+try:
+    print("Press Ctrl-C to stop")
+    while True:
+        r, g, b, c = tcs.color_raw
+        color_temp = tcs.color_temperature
+        lux = tcs.lux
+        
+        print("Color: ({0}, {1}, {2}, {3})".format(r, g, b, c))
+        print("Color Temperature: {0} K".format(color_temp))
+        print("Luminosity: {0} Lux".format(lux))
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("Stopped by User")
