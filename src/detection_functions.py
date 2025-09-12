a6140eaf86832adbc5faf4f49325cb38a8bd1786
_hazielvds <user.haziel@gmail.com>
2025-09-12 07:44:12 -0600

    Por finnn

--- src/detection_functions.py
+++ src/detection_functions.py
@@ -9,8 +9,8 @@ upper_2 = (32, 151, 224)
 x1, x2 = 200, 400
 y1, y2 = 400, 480
 
-last_callback = 1
-def trigger_line(hsv, frame, callback_1, callback_2):
+last_callback = 0
+def trigger_line(running, hsv, frame, callback_1, callback_2):
     global last_callback
     roi = hsv[y1:y2, x1:x2]
     cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
@@ -24,11 +24,15 @@ def trigger_line(hsv, frame, callback_1, callback_2):
     
     if count_1 > 500 and last_callback != 1:
         cv2.putText(frame, "Mask 1 Detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 165, 255), 2)
+        if not running:
+            return
         if callback_1:
             callback_1()
         last_callback = 1
     elif count_2 > 500 and last_callback != 2:
         cv2.putText(frame, "Mask 2 Detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 165, 255), 2)
+        if not running:
+            return
         if callback_2:
             callback_2()
         last_callback = 2
