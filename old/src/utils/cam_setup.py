80d93263c49b82565c8098a2f9ade819662d7b22
_hazielvds <user.haziel@gmail.com>
2025-09-11 21:26:54 -0600

    asd

--- old/src/utils/cam_setup.py
+++ old/src/utils/cam_setup.py
@@ -6,8 +6,8 @@ def nothing(x):
 
 # Cargar imagen o video
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 cap.set(cv2.CAP_PROP_FPS, 30)
 # Crear ventana
 cv2.namedWindow("Trackbars")
