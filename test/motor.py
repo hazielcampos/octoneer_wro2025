c7a3bfbff9c66d73c48a2b677e10b3a7a3000638
_hazielvds <user.haziel@gmail.com>
2025-09-13 13:12:17 -0600

    Improved motor test

--- test/motor.py
+++ test/motor.py
@@ -1,8 +1,62 @@
 
-from src.components.Motor import forward, backward, stop_motors, start as start_pwm
 import time
 import keyboard
-from src.components.Servo import set_angle, CENTER_POSITION, RIGHT_POSITION, LEFT_POSITION
+
+
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import servo
+
+import busio
+import board
+
+import Rasp.GPIO as GPIO
+import time
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+CENTER_POSITION = 52  # Neutral position of the servo
+LEFT_POSITION = 62 # Left position of the servo
+RIGHT_POSITION = 42  # Right position of the servo
+
+# =========================
+# PCA9685 and Servo setup
+# =========================
+pca = PCA9685(i2c, address=0x43)
+pca.frequency = 50  # Servo frequency
+direction_servo = servo.Servo(pca.channels[0])
+
+def set_angle(angle: float):
+    """Set the servo angle."""
+    if angle < 42 or angle > 62:
+        raise ValueError("Angle must be between 0 and 180 degrees.")
+    direction_servo.angle = angle
+
+def disable():
+    pca.deinit()  # Deinitialize PCA9685
+
+
+# =========================
+# GPIO setup
+# =========================
+FORWARD_IO = 12  # GPIO pin to move the motor forward
+BACKWARD_IO = 13  # GPIO pin to move the motor backward
+GPIO.setup(FORWARD_IO, GPIO.OUT)
+GPIO.setup(BACKWARD_IO, GPIO.OUT)
+FORWARD_PWM = GPIO.PWM(FORWARD_IO, 500)  #
+BACKWARD_PWM = GPIO.PWM(BACKWARD_IO, 500)  # 200 Hz
+def start_pwm():
+    FORWARD_PWM.start(0)
+    BACKWARD_PWM.start(0)
+
+def forward(speed):
+    BACKWARD_PWM.ChangeDutyCycle(0)
+    FORWARD_PWM.ChangeDutyCycle(speed)
+def backward(speed):
+    FORWARD_PWM.ChangeDutyCycle(0)
+    BACKWARD_PWM.ChangeDutyCycle(speed)
+def stop_motors():
+    FORWARD_PWM.ChangeDutyCycle(0)
+    BACKWARD_PWM.ChangeDutyCycle(0)
 
 
 speed = 30
