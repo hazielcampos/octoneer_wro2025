12fcc5b7f6c2944e702496e31998764ef944952b
_hazielvds <user.haziel@gmail.com>
2025-09-13 13:02:26 -0600

    Improved motor test

--- test/motor.py
+++ test/motor.py
@@ -1,20 +1,39 @@
 
-from src.components.Motor import forward, stop_motors, start as start_pwm
+from src.components.Motor import forward, backward, stop_motors, start as start_pwm
 import time
+import keyboard
+from src.components.Servo import set_angle, CENTER_POSITION, RIGHT_POSITION, LEFT_POSITION
 
+
+speed = 30
+def increase_speed(amount):
+    global speed
+    speed = min(speed + amount, 100)
+
+def decrease_speed(amount):
+    global speed
+    speed = max(speed - amount, 10)
 
 def main():
     start_pwm()
     while True:
-        forward(5)
-        input("Press Enter to next speed...")
-        forward(10)
-        input("Press Enter to next speed...")
-        forward(20)
-        input("Press Enter to next speed...")
-        forward(40)
-        input("Press Enter to next speed...")
-        
+        if keyboard.is_pressed("w"):
+            forward(speed)
+        elif keyboard.is_pressed("s"):
+            backward(speed)
+        if keyboard.is_pressed("d"):
+            set_angle(RIGHT_POSITION)
+        elif keyboard.is_pressed("a"):
+            set_angle(LEFT_POSITION)
+        else:
+            set_angle(CENTER_POSITION)
+            
+        if keyboard.is_pressed("space"):
+            stop_motors()
+        if keyboard.is_pressed("e"):
+            increase_speed(10)
+        elif keyboard.is_pressed("q"):
+            decrease_speed(10)
     
 try:
     main()
