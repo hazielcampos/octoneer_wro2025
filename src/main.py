f13b5e63ba30fc5bf341df75b5ba88708aa6d3fc
_hazielvds <user.haziel@gmail.com>
2025-09-14 14:05:32 -0600

    Added log improvements

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ from Logger import get_logger
 # ==============================
 TURN_THRESHOLD = 70
 NEXT_CURVE_THRESHOLD = 1.2
-TURN_END_DELAY = 0.8
+TURN_END_DELAY = 1
 
 
 # ==============================
